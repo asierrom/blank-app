8ac7e0d1f68b15460c1c25d8aceae9fb644a98b6
asierrom <asier.romero@outlook.es>
2026-03-06 20:56:14 +0100

    Web provisional Entrega 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,26 @@
 import streamlit as st
-import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
 
-df
+st.set_page_config(page_title="Desxifra'm!", page_icon="🔐")
+
+st.title("🔐 Desxifra'm!")
+st.subheader("Simulador de Protocols Criptogràfics")
+
+# Missatge d'estat
+st.info(" **Estat del projecte:** En desenvolupament .")
+
+# Contingut provisional
+st.markdown("""
+### PÀGINA EN DESENVOLUPAMENT
+
+---
+""")
+
+with st.expander("Missatge"):
+    st.write("""
+    Si no veus la web encara no és perquè no estigui acabada sinó perquè segons el principi d'incertesa no es pot veure el seu contingut i estar dins d'aquesta alhora.
+    """)
+
+st.write("Progrés de la web:")
+st.progress(35)
+
+st.caption("Treball de Recerca 2025-2026, Asier Romero | Desenvolupat amb Python i Streamlit")
